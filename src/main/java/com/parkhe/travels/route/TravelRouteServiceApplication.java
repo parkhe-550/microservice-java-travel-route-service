7c05b6d9dd297dc1b558fbedd577d2c20bb2bc52
parkhe-550 <prof.parkhe.550@gmail.com>
2023-02-25 17:33:43 +0530

    project setup

---
+++ src/main/java/com/parkhe/travels/route/TravelRouteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.parkhe.travels.route;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelRouteServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelRouteServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkhe/travels/route/TravelRouteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkhe.travels.route;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelRouteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
